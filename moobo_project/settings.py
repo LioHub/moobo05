19ccf2075894ceeb98a3f60ffc33295ac5407a16
liohub <liokncukles@gmail.com>
2020-07-06 19:57:26 +0300

    'lastchanges'

--- moobo_project/settings.py
+++ moobo_project/settings.py
@@ -28,7 +28,6 @@ STATICFILE_DIR = os.path.join(BASE_DIR, 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
